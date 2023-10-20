25975bf327652dfcaf1be49f6f973bbacbd4eb00
Elisei <elisey.rav@gmail.com>
2023-10-20 21:23:21 +0200

    feat: add gunicorn config

---
+++ gunicorn_config.py
@@ -0,0 +1,8 @@
+import multiprocessing
+
+# info: https://docs.gunicorn.org/en/stable/settings.html
+
+bind = "0.0.0.0:8000"
+#workers = multiprocessing.cpu_count() * 2 + 1
+workers = 2
+max_requests = 1000
