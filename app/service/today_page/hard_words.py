8bdd8222de6fbceae8c5fcfdf23266717170a183
Elisei <elisey.rav@gmail.com>
2023-11-09 22:08:44 +0100

    feat(today-page): Add ability to get hard words for today or yesterday
    - Added `PickDay` enum to represent options for picking a day (TODAY or YESTERDAY).
    - Modified `get_hard_words` function in `hard_words.py` to accept `pick_day` parameter and retrieve words based on the selected day.
    - Modified `get_today_page` function in `page.py` to use `WordPicker` to check if there are still words for learning, and based on that, retrieve hard words for either yesterday or today.

--- app/service/today_page/hard_words.py
+++ app/service/today_page/hard_words.py
@@ -1,4 +1,5 @@
 import datetime
+import enum
 
 from django.db.models import Count
 
@@ -6,17 +7,26 @@ from app.models import LearningHistory  # type: ignore[attr-defined]
 from app.service.today_page.schema import Word
 
 
-def get_hard_words(user_id: int) -> list[Word]:
+class PickDay(enum.Enum):
+    TODAY = enum.auto()
+    YESTERDAY = enum.auto()
+
+
+def get_hard_words(user_id: int, pick_day: PickDay) -> list[Word]:
     """
     Get words learned today.
     Sorted by number of attempts to learn it.
     """
     words_limit = 15
-    today = datetime.date.today() - datetime.timedelta(days=1)
+    if pick_day == PickDay.TODAY:
+        day = datetime.date.today()
+    else:
+        day = datetime.date.today() - datetime.timedelta(days=1)
+
     filter_by_command = "LEARNING_HARD"  # todo добавить еще статусы
 
     query_result = (
-        LearningHistory.objects.filter(created_at__date=today, word__user_id=user_id, command=filter_by_command)
+        LearningHistory.objects.filter(created_at__date=day, word__user_id=user_id, command=filter_by_command)
         .values("word__native", "word__foreign")
         .annotate(command_count=Count("id"))
         .order_by("-command_count")[:words_limit]
--- app/service/today_page/page.py
+++ app/service/today_page/page.py
@@ -1,10 +1,15 @@
-from .hard_words import get_hard_words
+from ..word import WordPicker
+from .hard_words import PickDay, get_hard_words
 from .schema import TodayPage
 from .text_generator import generate_today_text
 
 
 def get_today_page(user_id: int) -> TodayPage | None:
-    words = get_hard_words(user_id)
+    # if we still have words for learning, we take words from yesterday
+    if WordPicker().get_word_for_learning(user_id) is not None:
+        words = get_hard_words(user_id, pick_day=PickDay.YESTERDAY)
+    else:
+        words = get_hard_words(user_id, pick_day=PickDay.TODAY)
 
     if not words:
         return None
